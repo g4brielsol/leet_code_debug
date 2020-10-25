ace3158b71ca403ff49280431723cea653648037
gabriel sol <gabsolsilva@gmail.com>
2020-10-24 23:23:09 -0300

    second array exercise

---
+++ array2.py
@@ -0,0 +1,23 @@
+# 2 is even
+# 3 is odd
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        if (len(nums) >= 1) and (len(nums) <= 500):  
+            even_digits = 0
+            for number in nums:
+                if (number >= 1) and (number <= 10**5):
+                    digit_counter = 0
+                    while(number != 0):
+                        digit_counter += 1
+                        number = number // 10
+                    if digit_counter % 2 == 0:
+                        even_digits += 1
+                    else:
+                        pass
+                else:
+                    return False
+        else:
+            return False
+
+        return even_digits
+    
