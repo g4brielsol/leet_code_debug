ba7ffab5a7501fee71ee545a28925ee0231a7237
gabriel sol <gabsolsilva@gmail.com>
2020-10-26 22:17:12 -0300

    duplicate 0 and shift right

---
+++ array4.py
@@ -0,0 +1,36 @@
+class Solution:
+    def duplicateZeros(self, arr: list) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        if (len(arr) >= 1) and (len(arr) <= 10000):
+            #                       doesnt go to the last one
+            size = len(arr)
+            number = 0
+            while number < len(arr):
+                if (arr[number] >= 0) and (arr[number] <= 9):
+                    if arr[number] == 0:
+                        arr = arr[0:number+1] + [0] + arr[number+1:]
+                        arr = arr[0:size]
+                        number += 1
+                else:
+                    return False
+                number += 1
+        else:
+            return False
+        print(arr)
+        
+        
+        #[1,2,3,0,0,4,5,6,0,8,9]
+        
+        #[1,2,3,0,4,5,6,0,8,9]
+        #[1,2,3,0,0,4,5,6,0,0]
+        #[1,0,2,3]
+        #[1,0,0,2,3]
+        #[1,0,0,2]
+        #[1,2,3]
+
+solucao = Solution()
+solucao.duplicateZeros([1,0,2,3,0,4,5,0])
+#solucao.duplicateZeros([1,2,3])
+        
